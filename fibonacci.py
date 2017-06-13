1180591b142abae02ba7b6e45f1414505e6de297
Sami Douba <doubasami@gmail.com>
2017-06-13 14:12:29 -0400

    C4: Changed Fibo function to iterative version

--- fibonacci.py
+++ fibonacci.py
@@ -1,8 +1,8 @@
 def fibo(N):
-	if N<=1:
-		return N
-	else:
-		return fibo(N-1)+fibo(N-2)
+	a, b = 0, 1
+	for _ in range(N):
+		a, b = b, a+b
+	return a
 
 if __name__=="__main__":
 	print "Fibo(5)" + str(fibo(5))
